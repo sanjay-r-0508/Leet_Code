f4a439364df186929d098098da600fba70c6b2a6
Sanjay R <sanjayrao0508@gmail.com>
2025-04-19 11:21:05 +0530

    Time: 23 ms (100%), Space: 57.5 MB (41.82%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
@@ -1,51 +1,18 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
-        Arrays.sort(nums);  // Step 1: Sort the array
-        long count = 0;
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            // Step 2: Use binary search to find the valid range for nums[i]
-            int left = lowerBound(nums, i + 1, n - 1, lower - nums[i]);
-            int right = upperBound(nums, i + 1, n - 1, upper - nums[i]);
-
-            if (left <= right) {
-                count += right - left + 1;
-            }
-        }
-
-        return count;
+        Arrays.sort(nums);
+        return count(nums, upper) - count(nums, lower-1); 
     }
 
-    // Finds the first index in [start, end] such that nums[index] >= target
-    private int lowerBound(int[] nums, int start, int end, int target) {
-        int l = start, r = end;
-        int ans = end + 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] >= target) {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
+    private long count(int[] nums, int target){
+        long res = 0;
+        int i =0, j=nums.length-1;
+        while(i<j){
+            if(nums[i]+nums[j]>target)
+                j--;
+            else
+                res = res + j - i++;
         }
-        return ans;
-    }
-
-    // Finds the last index in [start, end] such that nums[index] <= target
-    private int upperBound(int[] nums, int start, int end, int target) {
-        int l = start, r = end;
-        int ans = start - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] <= target) {
-                ans = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        return ans;
+        return res;
     }
 }
